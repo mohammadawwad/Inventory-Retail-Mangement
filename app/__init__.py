57a32bdf3a31b73b60c3f1caada590593a2198c1
mohammadawwad <mohd-awwad@hotmail.com>
2022-10-22 18:55:16 -0700

    finish lat half of routes.py and the helpers folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
+from app.config import Config
+from app import routes
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+
+app.config.from_object(Config)
+app.static_folder = "static"
+
+
+
---
+++ helpers/user_helpers.py
@@ -0,0 +1,93 @@
+from pymysql.cursors import DictCursor
+from flaskext.mysql import MySQL
+from flask import Flask, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+Session(app)
+
+DATABASE_NAME = "InventoryRentalMangement"
+
+mysql = MySQL(
+    app,
+    prefix="my_database",
+    host="localhost",
+    user="test",
+    password="test",
+    db="InventoryRentalMangement",
+    autocommit=True,
+)
+
+
+def validateuser(details):
+    try:
+        username = details["username"]
+        password = details["password"]
+        cur = mysql.get_db().cursor(DictCursor)
+        print("inside validateuser")
+        cur.execute(
+            "SELECT RoleID FROM users where username=%s and password=%s",
+            (username, password),
+        )
+        userlist = cur.fetchall()
+        print("userlist ", userlist)
+        cur.close()
+        session["username"] = username
+        return userlist
+    except mysql.get_db().cursor().DatabaseError as e:
+        print(e)
+        return e
+
+
+def getUserByID(username):
+    # getUserByID can be combined with validateuser if needed
+    try:
+        cur = mysql.get_db().cursor(DictCursor)
+        cur.execute("SELECT * FROM users where username=%s", username)
+        userlist = cur.fetchall()
+        print("userlist ", userlist)
+        cur.close()
+        return userlist
+    except mysql.get_db().cursor().DatabaseError as e:
+        print(e)
+        return e
+
+
+def getUsers():
+
+    try:
+        cur = mysql.get_db().cursor(DictCursor)
+        cur.execute("SELECT * FROM users")
+        userlist = cur.fetchall()
+        print("userlist ", userlist)
+        cur.close()
+        return userlist
+    except mysql.get_db().cursor().DatabaseError as e:
+        print(e)
+        return e
+
+
+def insertnewuser(details):
+
+    print(details)
+    try:
+        roleType = details["roleType"]
+        userID = details["userID"]
+        fName = details["fName"]
+        lName = details["lName"]
+        password = details["password"]
+        email = details["email"]
+        ph = details["ph"]
+        cur = mysql.get_db().cursor()
+        cur.execute(
+            "INSERT INTO Users(UserName,Password,FirstName,Lastname,EmailID,Phone, RoleID) VALUES (%s, %s,%s,%s,%s,%s,%s)",
+            (userID, password, fName, lName, email, ph, roleType),
+        )
+        mysql.get_db().commit()
+        cur.close()
+        return "User " + userID + " successfully created.."
+    except mysql.get_db().cursor().DatabaseError as e:
+        print(e)
+        return e
